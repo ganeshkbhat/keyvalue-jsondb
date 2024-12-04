b1bc2b235f2a1de730ad08776ace0bf3307aad79
Ganesh K Bhat <ganeshsurfs@gmail.com>
2024-12-04 15:56:51 +0530

    [COMMIT] Adding first demo files

--- index.js
+++ index.js
@@ -38,46 +38,6 @@ function unflattenJson(obj) {
     return result;
 }
 
-// // Example Usage:
-// const flatData1 = { "test.tester.testing": "10" };
-// console.log(JSON.stringify(unflattenJson(flatData1), null, 2));
-// // Output:
-// // {
-// //   "test": {
-// //     "tester": {
-// //       "testing": "10"
-// //     }
-// //   }
-// // }
-
-// const flatData2 = {
-//     "test.tester.testing": "10",
-//     "test.tester.makeup": 10,
-//     "test.madeup": 20,
-//     "fakeup": 30
-// };
-// console.log(JSON.stringify(unflattenJson(flatData2), null, 2));
-// // Output:
-// // {
-// //   "test": {
-// //     "tester": {
-// //       "testing": "10",
-// //       "makeup": 10
-// //     },
-// //     "madeup": 20
-// //   },
-// //   "fakeup": 30
-// // }
-
-// const flatData3 = { "nest\\..secondnest.third\\.nest": 1 };
-// console.log(JSON.stringify(unflattenJson(flatData3), null, 2));
-// // Output:
-// // {
-// //   "nest.secondnest": {
-// //     "third.nest": 1
-// //   }
-// // }
-
 
 /**
  * Flattens a nested JSON object into a single level with dot notation keys,
@@ -118,43 +78,6 @@ function flattenJsonWithEscaping(obj, prefix = "") {
     recurse(obj, prefix);
     return result;
 }
-
-// // Example Usage:
-// const nestedData1 = { test: { tester: { testing: "10" } } };
-// console.log(flattenJsonWithEscaping(nestedData1));
-// // Output: { "test.tester.testing": "10" }
-
-// const nestedData2 = {
-//     test: {
-//         tester: {
-//             testing: "10",
-//             makeup: 10
-//         },
-//         madeup: 20
-//     },
-//     fakeup: 30
-// };
-// console.log(flattenJsonWithEscaping(nestedData2));
-// // Output: { "test.tester.testing": "10", "test.tester.makeup": 10, "test.madeup": 20, "fakeup": 30 }
-
-// const nestedData3 = {
-//     test: {
-//         "tester.makeup": {
-//             testing: "10",
-//             makeup: 10
-//         },
-//         madeup: 20
-//     },
-//     fakeup: 30
-// };
-// console.log(flattenJsonWithEscaping(nestedData3));
-// // Output:
-// // {
-// //   "test.tester\\.makeup.testing": "10",
-// //   "test.tester\\.makeup.makeup": 10,
-// //   "test.madeup": 20,
-// //   "fakeup": 30
-// // }
 
 
 class JsonManager {
