6de6e742175df5ce44fe9c2cf7c4227f253ab32e
Ganesh K Bhat <ganeshsurfs@gmail.com>
2025-03-19 00:46:30 +0530

    [COMMIT] Adding minor changes in implementation to shell.js

--- shell.js
+++ shell.js
@@ -17,9 +17,41 @@
 'use strict';
 
 const Shell = require('./index').Shell;
-const port = 3443, ip = "127.0.0.1";
+
+// 
+// const port = 3443, ip = "127.0.0.1";
+// process.argv is an array containing the command-line arguments
+// process.argv[0] is the Node.js executable path
+// process.argv[1] is the script file path
+// 
+// Example usage
+// node my_script.js arg1 arg2 arg3
+// output
+// Script Path: my_script.js
+// Arguments: [ 'arg1', 'arg2', 'arg3' ]
+//
+
+// arguments after the script name
+const args = process.argv.slice(2);
 
 // Add Shell Authentication here
 
-var srv = Shell(port, ip)
+var port, ip;
+
+if (!args[0]) {
+    console.log("Port is required, using port 3443");
+    port = 3443
+} else {
+    port = Number(args[0])
+}
+
+if (!args[1]) {
+    console.log("IP Online is required, using port 3443");
+    ip = "127.0.0.1";
+} else {
+    ip = args[1];
+}
+
+console.log("port: number ::", Number(args[0]), ", ip: string/text ::", args[1]);
+var srv = Shell(Number(args[0]), args[1]);
 
