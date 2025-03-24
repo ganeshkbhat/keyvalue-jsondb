0af9515cb4d63671475b48ddfb8746b5e1573b57
Ganesh K Bhat <ganeshsurfs@gmail.com>
2025-03-24 22:10:18 +0530

    [COMMIT] Adding minor changes to new shell code with shellflags package

--- shell.js
+++ shell.js
@@ -17,6 +17,7 @@
 'use strict';
 
 const Shell = require('./index').Shell;
+const shellflags = require("shellflags");
 
 // const RShell = require('./index').Shell;
 // var srv = RShell(port, ip, certkey)
@@ -35,39 +36,60 @@ const Shell = require('./index').Shell;
 // Arguments: [ 'arg1', 'arg2', 'arg3' ]
 //
 
-// arguments after the script name
+// // arguments after the script name
 const args = process.argv.slice(2);
 
-// Add Shell Authentication here
+// // Add Shell Authentication here
 
 var port, ip, certkey, username, password;
 
-if (!args[0]) {
-    console.log("Port is required, using port 3443");
-    port = 3443;
-} else {
-    port = Number(args[0])
-}
+// if (!args[0]) {
+//     console.log("Port is required, using port 3443");
+//     port = 3443;
+// } else {
+//     port = Number(args[0])
+// }
 
-if (!args[1]) {
-    console.log("IP Online is required, using port 3443");
-    ip = "127.0.0.1";
-} else {
-    ip = args[1];
-}
+// if (!args[1]) {
+//     console.log("IP Online is required, using port 3443");
+//     ip = "127.0.0.1";
+// } else {
+//     ip = args[1];
+// }
 
 
-if (!!args[2] && !!args[3]) {
-    console.log("");
-    user = args[2];
-    pass = args[3];
-}
+// if (!!args[2] && !!args[3]) {
+//     console.log("");
+//     user = args[2];
+//     pass = args[3];
+// }
 
-if (!!args[2] && !args[3]) {
-    certkey = args[2];
-}
+// if (!!args[2] && !args[3]) {
+//     certkey = args[2];
+// }
 
-console.log("port: number ::", Number(args[0]), ", ip: string/text ::", args[1]);
+const prefixDefinitions = [
+    { prefix: "-p", handler: () => console.log },
+    { prefix: "-s", handler: () => console.log },
+    { prefix: "-ip", handler: () => console.log },
+    { prefix: "-c", handler: () => console.log },
+    { prefix: "-u", handler: () => console.log },
+    { prefix: "-pwd", handler: () => console.log }
+];
+
+var results = shellflags(prefixDefinitions)
+console.log(results);
+
+console.log("port: number ::", Number(args[0]), ", ip: string/text ::", args[1],  "certkey: string/text ::", args[2], "username: string/text ::", args[3], "password: string/text ::", args[4]);
+console.log("port: number ::", results["-p"], ", ip: string/text ::", results["-ip"],  "certkey: string/text ::", results["-c"], "username: string/text ::", results["-u"], "password: string/text ::", results["-pwd"]);
+
+port = results["-p"] || 3443
+ip = results["-ip"] || "127.0.0.1"
+certkey = results["-c"]
+username = results["-u"]
+password = results["-pwd"]
+
+console.log("port: number ::", port, ", ip: string/text ::", ip,  "certkey: string/text ::", certkey, "username: string/text ::", username, "password: string/text ::", password);
 
 var srv;
 
