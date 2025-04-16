f72d00c11a158838e10042cd550683f52375b5a1
Ganesh K Bhat <ganeshsurfs@gmail.com>
2025-04-16 16:22:37 +0530

    [COMMIT] Adding minor changes to server and shell code for database

--- server.js
+++ server.js
@@ -18,13 +18,43 @@
 
 const startServer = require('./index').startServer;
 
-var type = (process.argv.length > 2 && !!process.argv[2]) ? process.argv[2] : "http"
-var port = (process.argv.length > 3 && !!process.argv[3]) ? process.argv[3] : 3443
-var ip = (process.argv.length > 4 && !!process.argv[4]) ? process.argv[4] : "127.0.0.1"
-var middlewares = []
-var app = (req, res, next) => { next() }
-var key = (process.argv.length > 5 && !!process.argv[5]) ? process.argv[5] : null 
-var cert = (process.argv.length > 6 && !!process.argv[6]) ? process.argv[6] : null
+// // USAGE
+// node server.js type port ip key cert
+// node server.js -t type -p port -ip ip -k key -c cert
+
+
+const prefixDefinitions = [
+    { prefix: "-t", handler: () => console.log },
+    { prefix: "-p", handler: () => console.log },
+    { prefix: "-ip", handler: () => console.log },
+    { prefix: "-k", handler: () => console.log },
+    { prefix: "-c", handler: () => console.log },
+];
+
+var results = shellflags(prefixDefinitions)
+console.log(results);
+
+console.log("type: number ::", results["-t"], "port: number ::", results["-p"], ", ip: string/text ::", results["-ip"], "certkey: string/text ::", results["-k"], "username: string/text ::", results["-c"]);
+
+var middlewares = [];
+var app = (req, res, next) => { next() };
+
+if (!results || results["-t"] || results["-p"] || results["-ip"] || results["-k"] || results["-c"]) {
+    results = {
+        "-t": (process.argv.length > 2 && !!process.argv[2]) ? process.argv[2] : "http",
+        "-p": (process.argv.length > 3 && !!process.argv[3]) ? process.argv[3] : 3443,
+        "-ip": (process.argv.length > 4 && !!process.argv[4]) ? process.argv[4] : "127.0.0.1",
+        "-k": (process.argv.length > 5 && !!process.argv[5]) ? process.argv[5] : null,
+        "-c": (process.argv.length > 6 && !!process.argv[6]) ? process.argv[6] : null
+    }
+}
+
+var type = results["-t"] || "http";
+var port = results["-p"] || 3443;
+var ip = results["-ip"] || "127.0.0.1";
+var key = results["-k"] || null;
+var cert = results["-c"] || null;
 
 var srv = startServer(type, port, ip, middlewares, app, key, cert);
 console.log("Running server at: ", `${type}, ${port}, ${ip}`)
+
